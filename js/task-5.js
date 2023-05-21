7aeab7108cb2f6a732adef6528f59014350130a3
Anatoly Kostrykin <gupekmop777@mail.ru>
2023-05-21 14:19:36 +0300

    Delete icon and text-block with hint

--- js/task-5.js
+++ js/task-5.js
@@ -1,54 +1,44 @@
 document.addEventListener('DOMContentLoaded', chatStart);
-
+// запускается после полной загрузки HTML
 function chatStart() {
-	const chatOutput = document.querySelector('.chat__output');
-	const apiUri = 'wss://echo-ws-service.herokuapp.com/';
-	const inputField = document.querySelector('.control-panel__input');
-	const chatHint = document.querySelector('.chat__hint');
 	const sendBtn = document.querySelector('.btn_send');
 	const geoBtn = document.querySelector('.btn_geolocation');
+	sendBtn.addEventListener('click', sendMessage); // при отправке сообщения
+	geoBtn.addEventListener('click', sendGeolocation); // при получении геоданных
+	// создаем новое соединение с сервером
+	let websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
+	startEvent();
+	// отслеживает события текущего соединения
+	function startEvent() {
+		const chatOutput = document.querySelector('.chat__output');
 
-	sendBtn.addEventListener('click', sendMessage);
-	geoBtn.addEventListener('click', sendGeolocation);
+		websocket.addEventListener('open', () => { //  соединение открыто
+			showConnectStatus('установлено', 'green'); // показываем статус
+		});
 
-	let websocket = new WebSocket(apiUri); // создаем новое соединение с сервером
+		websocket.addEventListener('close', () => { // соединение закрыто
+			showConnectStatus('закрыто', 'red'); // показываем статус
+			chatOutput.innerHTML = ''; // очищаем экран с собщениями
+			//chatStart(); // заново подключаем
+		});
 
-	websocket.addEventListener('open', () => { //  соединение открыто
-		showConnectStatus('установлено', 'green'); // показываем статус
-		if (chatHint) chatHint.remove(); // удаляем подсказку с экрана
-	});
+		websocket.addEventListener('error', () => { // ошибки соединения
+			showConnectStatus('ошибка! не установлено', 'red'); // показываем статус
+		});
 
-	websocket.addEventListener('message', (event) => { // сообщение получено
-		showContent(event.data);
-		// выводит полученные с сервера данные на экран
-		function showContent(message) {
-			let html, type;
-			if (event.data.includes('www' || 'http')) {
-				type === 'link'; // выводим на экран ссылку на местоположение
-				html = `<p class="chat__sent-message completed"> <a href="${message}"
-					target="_blank">Посмотреть местоположение</a></p>`;
-			} else if (event.data.includes('Ошибка!')) {
-				type === 'error'; // выводим сообщение об ошибке получения геоданных 
-				html = `<p class="chat__sent-message error">${message}</p>`
-			} else { // выводим на экран введенное в инпут сообщение
-				type === 'double';
-				html = `<p class="chat__sent-message">${message}</p>
-					<p class="chat__get-message">${message}</p>`;
-			}
-			// выводим соответствующее сообщение на экран
-			chatOutput.insertAdjacentHTML('beforeend', html);
-		}
-	});
-
+		websocket.addEventListener('message', (event) => { // сообщение получено
+			showContent(chatOutput, event.data);
+		});
+	}
 	// отправляет введенное сообщение серверу 
 	function sendMessage() {
+		const inputField = document.querySelector('.control-panel__input');
 		if (inputField.value !== '') { // если инпут не пустой
 			websocket.send(inputField.value); // отправляем сообщение
 			showConnectStatus('сообщение получено', 'green'); // показываем статус
 			inputField.value = ''; // очищаем инпут
 		}
 	}
-
 	// запрашивает геоданные, отправляет серверу
 	function sendGeolocation() {
 		if ('geolocation' in navigator) { // если браузер поддерживает определение геолокации
@@ -78,22 +68,28 @@ function chatStart() {
 			showConnectStatus('сообщение получено', 'green'); // показываем статус
 		}
 	}
-
+	// выводит полученные с сервера данные на экран
+	function showContent(output, message) {
+		let html, type;
+		if (message.includes('www' || 'http')) {
+			type === 'link'; // выводим на экран ссылку на местоположение
+			html = `<p class="chat__sent-message completed"> <a href="${message}"
+				target="_blank">Посмотреть местоположение</a></p>`;
+		} else if (message.includes('Ошибка!')) {
+			type === 'error'; // выводим сообщение об ошибке получения геоданных 
+			html = `<p class="chat__sent-message error">${message}</p>`
+		} else { // выводим на экран введенное в инпут сообщение
+			type === 'double';
+			html = `<p class="chat__sent-message">${message}</p>
+				<p class="chat__get-message">${message}</p>`;
+		}
+		// выводим соответствующее сообщение на экран
+		output.insertAdjacentHTML('beforeend', html);
+	}
 	// показывает текущий статус соединения 
 	function showConnectStatus(text, color) {
 		const statusOutput = document.querySelector('.chat__status-output');
 		const html = `<p>Состояние соединения: <span class="color-${color}"> ${text}</span></p>`;
 		statusOutput.innerHTML = html;
 	}
-
-	websocket.addEventListener('close', () => { // соединение закрыто
-		showConnectStatus('закрыто', 'red'); // показываем статус
-		chatOutput.innerHTML = ''; // очищаем экран с собщениями
-		const hintHtml = chatHint.outerHTML; // выводим подсказку вновь
-		chatOutput.innerHTML = hintHtml;
-	});
-
-	websocket.addEventListener('error', () => { // ошибки соединения
-		showConnectStatus('ошибка! не установлено', 'red'); // показываем статус
-	});
 }
