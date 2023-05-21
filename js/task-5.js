5dd37f85de1baf40cc5ba0880f3a470a42c15c55
Anatoly Kostrykin <gupekmop777@mail.ru>
2023-05-21 17:02:40 +0300

    code style changed

--- js/task-5.js
+++ js/task-5.js
@@ -1,39 +1,30 @@
-document.addEventListener('DOMContentLoaded', chatStart);
-// запускается после полной загрузки HTML
+document.addEventListener('DOMContentLoaded', chatStart); // запускается после полной загрузки HTML
 function chatStart() {
+	const chatOutput = document.querySelector('.chat__output');
 	const sendBtn = document.querySelector('.btn_send');
 	const geoBtn = document.querySelector('.btn_geolocation');
+	let websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
 	sendBtn.addEventListener('click', sendMessage); // при отправке сообщения
 	geoBtn.addEventListener('click', sendGeolocation); // при получении геоданных
-	// создаем новое соединение с сервером
-	let websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
-	startEvent();
-	// отслеживает события текущего соединения
-	function startEvent() {
-		const chatOutput = document.querySelector('.chat__output');
-
-		websocket.addEventListener('open', () => { //  соединение открыто
-			showConnectStatus('установлено', 'green'); // показываем статус
-		});
-
-		websocket.addEventListener('close', () => { // соединение закрыто
-			showConnectStatus('закрыто', 'red'); // показываем статус
-			chatOutput.innerHTML = ''; // очищаем экран с собщениями
-			chatStart(); // заново все подключаем
-		});
-
-		websocket.addEventListener('error', () => { // ошибки соединения
-			showConnectStatus('ошибка! не установлено', 'red'); // показываем статус
-		});
-
-		websocket.addEventListener('message', (event) => { // сообщение получено
-			showContent(chatOutput, event.data);
-		});
-	}
-	// отправляет введенное сообщение серверу 
+	showConnectStatus('закрыто', 'red'); // показываем статус
+	// создаем новое соединение с сервером и отслеживаем его события
+	websocket.addEventListener('open', () => { // соединение открыто 
+		showConnectStatus('установлено', 'green'); // показываем статус
+	});
+	websocket.addEventListener('message', (event) => { // сообщение получено
+		showContent(chatOutput, event.data); // выводим его на экран
+	});
+	websocket.addEventListener('close', () => { // соединение закрыто
+		showConnectStatus('закрыто', 'red'); // показываем статус
+		websocket = null;
+	});
+	websocket.addEventListener('error', () => { // ошибка соединения 
+		showConnectStatus('не установлено', 'red'); // показываем статус
+	});
+	// отправляет введенное сообщение на сервер 
 	function sendMessage() {
-		const inputField = document.querySelector('.control-panel__input');
-		if (inputField.value !== '') { // если инпут не пустой
+		let inputField = document.querySelector('.control-panel__input');
+		if (inputField.value !== '' && websocket) { // если инпут не пустой
 			websocket.send(inputField.value); // отправляем сообщение
 			showConnectStatus('сообщение получено', 'green'); // показываем статус
 			inputField.value = ''; // очищаем инпут
@@ -41,50 +32,40 @@ function chatStart() {
 	}
 	// запрашивает геоданные, отправляет серверу
 	function sendGeolocation() {
-		if ('geolocation' in navigator) { // если браузер поддерживает определение геолокации
-			const options = { enableHighAccuracy: true }; // для более точного поиска
-			navigator.geolocation.getCurrentPosition(completed, error, options);
-
-			function completed(data) { // если пользователь дал согласие
-				const lat = data.coords.latitude;
-				const long = data.coords.longitude;
-				const url = new URL('https://www.openstreetmap.org'); // конструируем url
-				url.searchParams.set('mlat', lat);
-				url.searchParams.set('mlon', long);
-				websocket.send(url); // отправляем сообщение на сервер
-				showConnectStatus('сообщение получено', 'green'); // показываем статус
+		if ('geolocation' in navigator && websocket) { // если браузер поддерживает определение геолокации
+			navigator.geolocation.getCurrentPosition(completed, error, { enableHighAccuracy: true });
+			// если пользователь согласен предоставить координаты
+			function completed(data) { // конструируем и отправляем ссылку на сервер
+				const url = new URL('https://www.openstreetmap.org');
+				url.searchParams.set('mlat', data.coords.latitude);
+				url.searchParams.set('mlon', data.coords.longitude);
+				websocket.send(url);
 			}
-
-			function error(data) { // если пользователь отказался
-				const errorText = `Ошибка! Не удалось получить координаты:( <br>  
-							Причина: ${data.message}`;
-				websocket.send(errorText); // отправляем на сервер сообщение
-				showConnectStatus('сообщение получено', 'green'); // показываем статус
+			// если пользователь отказался отправляем на сервер сообщение
+			function error(data) {
+				websocket.send(`Ошибка! Координаты не получены :(<br>Причина: ${data.message}`);
 			}
-
-		} else { // передаем сообщение об ошибке 
-			const errorText = 'Ошибка! Браузер не поддерживает геолокацию :(';
-			websocket.send(errorText); // отправляем сообщение на сервер
-			showConnectStatus('сообщение получено', 'green'); // показываем статус
+			showConnectStatus('сообщение получено', 'green'); // показываем статус 
+		} else { // передаем сообщение об ошибке на сервер 
+			if (websocket) {
+				websocket.send('Ошибка! Браузер не поддерживает геолокацию :(');
+				showConnectStatus('сообщение получено', 'green'); // показываем статус 
+			}
 		}
 	}
 	// выводит полученные с сервера данные на экран
 	function showContent(output, message) {
-		let html, type;
+		let html;
 		if (message.includes('www' || 'http')) {
-			type === 'link'; // выводим на экран ссылку на местоположение
 			html = `<p class="chat__sent-message completed"> <a href="${message}"
 				target="_blank">Посмотреть местоположение</a></p>`;
-		} else if (message.includes('Ошибка!')) {
-			type === 'error'; // выводим сообщение об ошибке получения геоданных 
+		} else if (message.includes('Ошибка' || 'error')) {
 			html = `<p class="chat__sent-message error">${message}</p>`
 		} else { // выводим на экран введенное в инпут сообщение
-			type === 'double';
 			html = `<p class="chat__sent-message">${message}</p>
 				<p class="chat__get-message">${message}</p>`;
 		}
-		// выводим соответствующее сообщение на экран
-		output.insertAdjacentHTML('beforeend', html);
+		output.insertAdjacentHTML('beforeend', html); // выводим соответствующее сообщение на экран
 	}
 	// показывает текущий статус соединения 
 	function showConnectStatus(text, color) {
