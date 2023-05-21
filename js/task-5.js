a0a803a9056ee4678ac3111873c340b284c60714
Anatoly Kostrykin <gupekmop777@mail.ru>
2023-05-21 17:16:43 +0300

    Code-style-changes

--- js/task-5.js
+++ js/task-5.js
@@ -1,18 +1,31 @@
 document.addEventListener('DOMContentLoaded', chatStart); // запускается после полной загрузки HTML
 function chatStart() {
-	const chatOutput = document.querySelector('.chat__output');
 	const sendBtn = document.querySelector('.btn_send');
 	const geoBtn = document.querySelector('.btn_geolocation');
-	let websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
 	sendBtn.addEventListener('click', sendMessage); // при отправке сообщения
 	geoBtn.addEventListener('click', sendGeolocation); // при получении геоданных
-	showConnectStatus('закрыто', 'red'); // показываем статус
 	// создаем новое соединение с сервером и отслеживаем его события
+	let websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
 	websocket.addEventListener('open', () => { // соединение открыто 
 		showConnectStatus('установлено', 'green'); // показываем статус
 	});
 	websocket.addEventListener('message', (event) => { // сообщение получено
-		showContent(chatOutput, event.data); // выводим его на экран
+		showContent(event.data); // выводим его на экран
+		// выводит полученные с сервера данные на экран
+		function showContent(message) {
+			const chatOutput = document.querySelector('.chat__output');
+			let html;
+			if (message.includes('www' || 'http')) {
+				html = `<p class="chat__sent-message completed"> <a href="${message}"
+				target="_blank">Посмотреть местоположение</a></p>`;
+			} else if (message.includes('Ошибка' || 'error')) {
+				html = `<p class="chat__sent-message error">${message}</p>`
+			} else { // выводим на экран введенное в инпут сообщение
+				html = `<p class="chat__sent-message">${message}</p>
+				<p class="chat__get-message">${message}</p>`;
+			}
+			chatOutput.insertAdjacentHTML('beforeend', html); // выводим соответствующее сообщение на экран
+		}
 	});
 	websocket.addEventListener('close', () => { // соединение закрыто
 		showConnectStatus('закрыто', 'red'); // показываем статус
@@ -52,20 +65,6 @@ function chatStart() {
 				showConnectStatus('сообщение получено', 'green'); // показываем статус 
 			}
 		}
-	}
-	// выводит полученные с сервера данные на экран
-	function showContent(output, message) {
-		let html;
-		if (message.includes('www' || 'http')) {
-			html = `<p class="chat__sent-message completed"> <a href="${message}"
-				target="_blank">Посмотреть местоположение</a></p>`;
-		} else if (message.includes('Ошибка' || 'error')) {
-			html = `<p class="chat__sent-message error">${message}</p>`
-		} else { // выводим на экран введенное в инпут сообщение
-			html = `<p class="chat__sent-message">${message}</p>
-				<p class="chat__get-message">${message}</p>`;
-		}
-		output.insertAdjacentHTML('beforeend', html); // выводим соответствующее сообщение на экран
 	}
 	// показывает текущий статус соединения 
 	function showConnectStatus(text, color) {
